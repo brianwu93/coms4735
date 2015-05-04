76fab265caa161b3e806bf90bf29e0c7d9528818
Brian Wu <brianwu93@gmail.com>
2015-05-04 18:03:08 -0400

    Initializing .m file for image processing functions.

---
+++ CVFunctions.m
@@ -0,0 +1,14 @@
+% Takes an HSV image and thresholds the image around a desired HSV color.
+% The resulting image is a binary image.
+function binary = getThresholdImage(image, target)
+
+% Find all pixels in the image within a range of the target values.
+H = target(1);
+S = target(2);
+V = target(3);
+
+binary = image(:,:,1) > (H - 0.1) & image(:,:,1) < (H + 0.1) &...
+         image(:,:,2) > (S - 0.1) & image(:,:,2) < (S + 0.1) &...
+         image(:,:,3) > (V - 0.1) & image(:,:,3) < (V + 0.1);
+
+end
