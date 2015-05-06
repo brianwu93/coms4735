73f2cebe66642e8ddd068cebe7744561e7149e70
Joshua Boggs <joshua.j.boggs@gmail.com>
2015-05-06 16:10:59 -0400

    combined function files

--- TestDrumSounds.m
+++ TestDrumSounds.m
@@ -14,3 +14,13 @@ function TestDrumSounds()
     pause(0.2);
     play(Tom);
 end
+
+% Locate drum sound files
+function [Crash, HfHat, Kick, Rim, Snare, Tom] = LocateDrumSounds()
+    Crash = 'DrumSounds/Crash.wav';
+    HfHat = 'DrumSounds/HfHat.wav';
+    Kick = 'DrumSounds/Kick.wav';
+    Rim = 'DrumSounds/Rim.wav';
+    Snare = 'DrumSounds/Snare.wav';
+    Tom = 'DrumSounds/Tom.wav';
+end
