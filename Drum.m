92f242d257bbd409af248dd63e29f1321f859b43
Joshua Boggs <joshua.j.boggs@gmail.com>
2015-05-07 16:22:34 -0400

    track drum hits on four separate regions:

--- Drum.m
+++ Drum.m
@@ -1,89 +1,129 @@
-function Drum(URL)
+function Drum()    
+    % Connect to the webcam.
+    vid = videoinput('macvideo', 1);
 
-    TestDrumSounds();
-  
-    sounds = LocateDrumSounds();
-    
-    XI = zeros(2,2);
-
-    % If it hit: size of orange ball
-    % where it hit: sectioned region of the image
-    
-    % Feature: Allow user to select sound for each pad
+    % Configure video settings.
+    set(vid,'FramesPerTrigger',1);
+    set(vid,'TriggerRepeat',Inf);
+    set(vid,'ReturnedColorSpace','rgb');
+    triggerconfig(vid, 'Manual');
+    fig = figure(1);
     
     % Delay a few seconds to allow camera repositioning
     pause(3);
     
+    % All sounds play to represent start
+    TestDrumSounds();
+    
     % Identify the color of the drum sticks to be tracked.
-    rgb_image = imread(URL);
+    rgb_image = getsnapshot(vid);
     imshow(rgb_image);
-    [x, y] = getpts;
+    [x, y] = ginput(1);
     hsv_image = rgb2hsv(rgb_image);
     target = impixel(hsv_image, x, y);
     
-    % Identify all 6 drum pads
+    % Identify the color of the drum pads to be tracked.
     imshow(rgb_image);
-    [a, b] = getpts;
-    X = [a, b];
-%     hsv_image = rgb2hsv(rgb_image);
-%     d = zeros(1,5);
-%     for n = 1:6
-%     	d(n) = impixel(hsv_image, a(n), b(n));
-%     end
-
-
+    [a, b] = ginput(4);
+    hsv_image = rgb2hsv(rgb_image);
+    pad_colors = zeros(1,4);
+    for n = 1:4
+    	pad_colors(n) = impixel(hsv_image, a(n), b(n));
+    end
     
-    % Play the instrument until the program is stopped.
-    %while 1
-        % Pull the image in HSV.
-       % hsv_image = rgb2hsv(snapshot(cam));
-        
-        % Convert image to binary to identify the sticks.
-        binary = getThresholdImage(hsv_image, target);
-        binary = getCleanImage(binary);
-        
-        % Erode to remove noise
-        se = strel('disk', 7);        
-        eroded = imerode(binary, se);
+    % Pull from the webcam and execute sound at a timed interval.
+    FPS = 10;
+    play = timer('TimerFcn', {@PlayDrums, vid, target, pad_colors}, 'Period', ...
+                 1/FPS, 'ExecutionMode', 'fixedRate', 'BusyMode', 'drop');
+             
+    % Begin pulling from the webcam.
+    start(vid);
+    start(play);
+    uiwait(fig);
     
-        % Segment the binary image so we can find the two drum sticks.
-        [labeled, num] = bwlabel(eroded);
-        if num == 2
-            blob1 = (labeled == 1);
-            blob2 = (labeled == 2);
-            XI(1,:) = findCentroid(blob1);
-            XI(2,:) = findCentroid(blob2);
-            XI;
-        elseif num < 2
-            imshow(eroded);
-            disp('Less than two drum sticks detected.');
-            %continue;
-        else
-              % computationally expensive...
-%             while num > 2
-%                 CC = bwconncomp(binary);
-%                 numPixels = cellfun(@numel,CC.PixelIdxList);
-%                 [~,idx] = min(numPixels);
-%                 binary(CC.PixelIdxList{idx}) = 0;
-%             end
-            imshow(eroded);
-            disp('More than two drum sticks detected.');
-            %continue;
+    % Clean up.
+    stop(play);
+    delete(play);
+    stop(vid);
+    delete(vid);
+    
+end
+
+function PlayDrums(obj, event, vid, target, pad_colors)
+    persistent image;
+    persistent current;
+    playSound = 1;
+    trigger(vid);
+    current = getdata(vid,1,'uint8');
+    binary_pads = zeros(1,4);
+    pad_centroids = zeros(4,2);
+    sounds = LocateDrumSounds();
+    
+    % Downsample and then convert raw data to HSV.
+    current = imresize(current, 0.5);
+    hsv_image = rgb2hsv(current);
+
+    % Convert image to binary to identify the sticks.
+    binary_stick = getSingleColorImage(hsv_image, target);
+
+%         % Erode to remove noise
+%         se = strel('disk', 7);        
+%         eroded = imerode(binary, se);
+
+    % Segment the binary image so we can find the two drum sticks.
+    [labeled, num] = bwlabel(binary_stick);
+    objects = zeros(num, 1);
+    for i = 1:num
+        objects(i) = sum(labeled(:) == i);
+    end
+    num_large_objects = sum(objects > 300);
+
+    if num_large_objects >= 2 % what if there is only one stick
+        [~, i] = max(objects);
+        objects(i) = 0;
+        [~, j] = max(objects);
+        blob1 = (labeled == i);
+        blob2 = (labeled == j);
+    end
+
+    [x1,y1] = findCentroid(blob1);
+    [x2,y2] = findCentroid(blob2);
+    XI = [x1,y1;x2,y2];
+
+    % Locate drum pads
+    for n = 1:4
+        binary_pads(n) = getSingleColorImage(hsv_image, pad_colors(n));
+        [labeled, num] = bwlabel(binary_pads(n));
+        objects = zeros(num, 1);
+        for i = 1:num
+            objects(i) = sum(labeled(:) == i);
         end
-        
-        %IDX = knnsearch(X,Y);
-        k = dsearchn(X,XI);
-        
-        
-        play(sounds(k(1),:));
-        play(sounds(k(2),:));
-        
-     
-        % Locate circles that correspond to different drum noises
-        % Locate drum stick ends
-        % If sticks and circles overlap, play sounds
-       
-        
-        
-   % end
+        num_large_objects = sum(objects > 300);
+
+        if num_large_objects >= 1
+            [~, i] = max(objects);
+            drumblob = (labeled == i);
+        end
+
+        [c1,c2] = findCentroid(drumblob);
+         pad_centroids(n,:) = [c1,c2];
+    end    
+
+    % Find closest pad centroids to drum stick centroids
+    k = dsearchn(pad_centroids,XI);
+
+    play(sounds(k(1),:));
+    play(sounds(k(2),:));  
+    
+    % Display the video showing only the tracking objects
+    if playSound
+        binary = blob1 + blob2;
+    end
+    if isempty(image)
+       image =imagesc(binary);
+       title('Drum CV Capture');
+    else
+       % Only update if needed.
+       set(image,'CData', binary);
+    end
 end
