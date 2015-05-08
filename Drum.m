5c806ae41bfd49ddcca9727ae3b39ea60d90a9a1
Brian Wu <brianwu93@gmail.com>
2015-05-08 04:02:51 -0400

    completed drum kit

--- Drum.m
+++ Drum.m
@@ -1,8 +1,23 @@
 function Drum()
-    % Get sounds
-    C = cell(1,3);
-    sounds = LocateDrumSounds();
-    C(1,3) = {sounds};
+    global MUSIC;
+    global NOTE_INDEX;
+    global COUNT;
+    global FPS;
+    global CENTROIDS;
+    COUNT = 0;
+    colors = zeros(5, 3);
+    % Preload sound components.
+    Crash = 'DrumSounds/1.wav';
+    HfHat = 'DrumSounds/2.wav';
+    Kick = 'DrumSounds/3.wav';
+    Rim = 'DrumSounds/4.wav';
+    sounds = [Crash; HfHat; Kick; Rim];
+    NOTE_INDEX = ['Crash', 'HfHat', 'Kick', 'Rim'];
+    MUSIC = cell(4);
+    for i = 1:4
+        [y,~] = audioread(sounds(i,:));
+        MUSIC{i} = y;
+    end
     
     % Connect to the webcam.
     vid = videoinput('macvideo', 1);
@@ -16,31 +31,51 @@ function Drum()
     fig = figure(1);
     
     % Delay a few seconds to allow camera repositioning
-    pause(3);
+    %pause(3);
     
     % All sounds play to represent start
-    TestDrumSounds();
+    %TestDrumSounds();
     
     % Identify the color of the drum sticks to be tracked.
     rgb_image = getsnapshot(vid);
     imshow(rgb_image);
     [x, y] = ginput(1);
     target = impixel(rgb2hsv(rgb_image), x, y);
-    C(1,1) = {target};
+    colors(1, :) = target(:);
     
     % Identify the color of the drum pads to be tracked.
     imshow(rgb_image);
     [a, b] = ginput(4);
     hsv_image = rgb2hsv(rgb_image);
-    pad_colors = zeros(4,3);
     for n = 1:4
-    	pad_colors(n,:) = impixel(hsv_image, a(n), b(n));
+    	color = impixel(hsv_image, a(n), b(n));
+        colors(n+1,:) = color(:);
     end
-    C(1,2) = {pad_colors};
+    
+    CENTROIDS = zeros(4,2);
+    % Downsample and then convert raw data to HSV.
+    hsv_image = imresize(hsv_image, 0.2);
+    for n = 1:4
+        binary_pad = getSingleColorImage(hsv_image, colors(n+1,:));
+        [labeled, num] = bwlabel(binary_pad);
+        objects = zeros(num, 1);
+        for i = 1:num
+            objects(i) = sum(labeled(:) == i);
+        end
+        num_large_objects = sum(objects > 300);
+
+        if num_large_objects >= 1
+            [~, i] = max(objects);
+            drumblob = (labeled == i);
+        end
+
+        [c1,c2] = findCentroid(drumblob);
+        CENTROIDS(n,:) = [c1,c2];
+    end
 
     % Pull from the webcam and execute sound at a timed interval.
-    FPS = 3;
-    play = timer('TimerFcn', {@PlayDrums, vid, C}, 'Period', ...
+    FPS = 20;
+    play = timer('TimerFcn', {@PlayDrums, vid, colors}, 'Period', ...
                  1/FPS, 'ExecutionMode', 'fixedRate', 'BusyMode', 'drop');
              
     % Begin pulling from the webcam.
@@ -56,33 +91,33 @@ function Drum()
     clear functions;
 end
 
-function PlayDrums(obj, event, vid, C)
+function PlayDrums(obj, event, vid, colors)
+    global MUSIC;
+    global COUNT;
+    global FPS;
+    global CENTROIDS;
     persistent image;
     persistent current;
     playSound = 1; %
-    twoSticks = 1; %
     trigger(vid);
     current = getdata(vid,1,'uint8');
-    binary_pads = zeros(1,4);
-    pad_centroids = zeros(4,2);
-    target = C{1,1};
-    pad_colors = C{1,2};
-    sounds = C{1,3};
-   
+    
+    if COUNT > 0
+        playSound = 0;
+        COUNT = COUNT - 1;
+    end
     % Downsample and then convert raw data to HSV.
     current = imresize(current, 0.2);
     hsv_image = rgb2hsv(current);
 
     % Convert image to binary to identify the sticks.
-    binary_stick = getThresholdImage(hsv_image, target);
+    binary_stick = getThresholdImage(hsv_image, colors(1,:));
     binary_stick = getCleanImage(binary_stick);
-
-%         % Erode to remove noise
-%         se = strel('disk', 7);        
-%         eroded = imerode(binary, se);
 
     % Segment the binary image so we can find the two drum sticks.
     [labeled, num] = bwlabel(binary_stick);
+    [m,n] = size(labeled);
+    binary_image = zeros(m,n);
     objects = zeros(num, 1);
     for i = 1:num
         objects(i) = sum(labeled(:) == i);
@@ -99,47 +134,32 @@ function PlayDrums(obj, event, vid, C)
         [~, j] = max(objects);
         blob1 = (labeled == i);
         blob2 = (labeled == j);
+        twoSticks = 1;
     else
         playSound = 0;
     end
-    
-    if playSound   
-         % Locate drum pads
-        for n = 1:4
-            binary_pads(n) = getSingleColorImage(hsv_image, pad_colors(n));
-            [labeled, num] = bwlabel(binary_pads(n));
-            objects = zeros(num, 1);
-            for i = 1:num
-                objects(i) = sum(labeled(:) == i);
-            end
-            num_large_objects = sum(objects > 300);
-
-            if num_large_objects >= 1
-                [~, i] = max(objects);
-                drumblob = (labeled == i);
-            end
-
-            [c1,c2] = findCentroid(drumblob);
-             pad_centroids(n,:) = [c1,c2];
-        end
-        
+    if playSound 
         % Find closest pad centroids to drum stick centroids
         % Display the video showing only the tracking objects
         if twoSticks
             [x1,y1] = findCentroid(blob1);
             [x2,y2] = findCentroid(blob2);
             XI = [x1,y1;x2,y2];
-            k = dsearchn(pad_centroids,XI);
-            play(sounds(k(1),:));
-            play(sounds(k(2),:));
+            k = dsearchn(CENTROIDS,XI);
+            y1 = MUSIC{k(1)};
+            y2 = MUSIC{k(2)};
+            sound(y1, 44100);
+            sound(y2, 44100);
             binary_image = blob1 + blob2;
         else
             [x1,y1] = findCentroid(blob1);
             XI = [x1,y1];
-            k = dsearchn(pad_centroids,XI);
-            play(sounds(k(1),:));
+            k = dsearchn(CENTROIDS,XI);
+            y = MUSIC{k(1)};
+            sound(y, 44100);
             binary_image = blob1;
-        end     
+        end
+        COUNT = FPS/5;
     end
     
     if isempty(image)
--- play.m
+++ play.m
@@ -2,7 +2,6 @@
 % input sound file i.e. filename = 'DrumSounds/Kick.wav';
 
 function play(filename)
-    load handel.mat
     [y,Fs] = audioread(filename);
     sound(y,Fs);
 end
